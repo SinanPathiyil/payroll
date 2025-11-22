c75dbdb35143ee6ca70c6f6b7fd6afb249b29c65
SinanPathiyil <sinanpathiyil07@gmail.com>
2025-11-22 20:54:33 +0530

    added new cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,8 @@ app = FastAPI(title="Employee Tracker API")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:3000",
+    allow_origins=[ "https://payroll-frontend-boehm.netlify.app/",
+                   "http://localhost:3000",
            "http://localhost:5173",
            "http://127.0.0.1:3000",
            "http://127.0.0.1:5173",

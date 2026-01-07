689651667d7120c045e3d1bff7c6d2e58059edde
SinanPathiyil <sinanpathiyil07@gmail.com>
2026-01-07 14:45:29 +0530

    uncommented layout and changed to Layout

--- frontend/src/components/common/Layout.jsx
+++ frontend/src/components/common/Layout.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
-/* import '../../styles/layout.css'; */
+import '../../styles/Layout.css';
 
 export default function Layout({ children }) {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
